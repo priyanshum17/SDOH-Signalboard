c37034e751aee9d3b1d54a7ea9b6b0fd9fe2b363
MeenakshiPrabhakar <115045523+MeenakshiPrabhakar@users.noreply.github.com>
2026-04-16 14:38:41 -0500

    Add Care Plan page to the application

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="SDOH Signal Board", layout="wide")
 
 dash = st.Page("pages/1_dashboard.py", title="Dashboard", default=True)
 editor = st.Page("pages/2_clinical_editor.py", title="Clinical Editor")
+care_plan = st.Page("pages/3_care_plan.py", title="Care Plan")
 
 pg = st.navigation([dash, editor])
 pg.run()
