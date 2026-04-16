f00d6745132edf225741f4985a97c083fe3b154f
MeenakshiPrabhakar <115045523+MeenakshiPrabhakar@users.noreply.github.com>
2026-04-16 14:46:03 -0500

    Add Care Plan page to navigation

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ dash = st.Page("pages/1_dashboard.py", title="Dashboard", default=True)
 editor = st.Page("pages/2_clinical_editor.py", title="Clinical Editor")
 care_plan = st.Page("pages/3_care_plan.py", title="Care Plan")
 
-pg = st.navigation([dash, editor])
+pg = st.navigation([dash, editor, care_plan])
 pg.run()
